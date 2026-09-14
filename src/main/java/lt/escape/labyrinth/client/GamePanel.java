ff9341fd5505535d72066d501ecc5b17a91318c1
Adrijus <adrijus998@gmail.com>
2026-09-14 18:06:02 +0300

    Added basic player rendering

--- src/main/java/lt/escape/labyrinth/client/GamePanel.java
+++ src/main/java/lt/escape/labyrinth/client/GamePanel.java
@@ -5,15 +5,29 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class GamePanel extends JPanel {
+    private final Player player;
+
     public GamePanel() {
         setBackground(Color.DARK_GRAY);
+
+        player = new Player(500,480);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        //ground
         g.setColor(Color.GREEN);
         g.fillRect(0, 520, getWidth(), 80);
+
+        //player
+        g.setColor(Color.BLUE);
+        g.fillRect(
+                (int) player.getX(),
+                (int) player.getY(),
+                40,
+                40
+        );
     }
 }
